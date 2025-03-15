e7b8496206a2e0fd275477bddd2ceceaddbb8e5d
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-14 22:22:00 -0400

    Add terraform required to main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,10 @@
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  s3_force_path_style         = true
-  skip_requesting_account_id  = true
+terraform {
+  required_version = ">= 0.12"
 
-  resource "aws_s3_bucket" "test-bucket" {
-    bucket = "my-bucket"
-  }
-
-  endpoints {
-    apigateway     = "http://localstack:4566"
-    ecs            = "http://localstack:4566"
-    rds            = "http://localstack:4566"
-    iam            = "http://localstack:4566"
-    ec2            = "http://localstack:4566"
-    # Add other services as needed
-    s3             = "http://s3.localhost.localstack.cloud:4566"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
-
- resource "aws_s3_bucket" "test-bucket" {
-    bucket = "my-bucket"
-  }
