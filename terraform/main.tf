c4023c84b32374b3d22c71aec2878c739e1e76e4
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-14 22:34:32 -0400

    Add provider config to main

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,23 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region                      = var.region
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = false
+
+  endpoints {
+    apigateway = "http://localstack:4566"
+    ecs        = "http://localstack:4566"
+    rds        = "http://localstack:4566"
+    iam        = "http://localstack:4566"
+    ec2        = "http://localstack:4566"
+    # Add other services as needed
+    s3 = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
