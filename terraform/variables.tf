a587d4bccbdfddf594af9ed42ea8fed39243027c
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-16 12:40:39 -0400

    Add terraform state & backup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,24 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
+# # VPC
+variable "base_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+  validation {
+    condition     = split("/", var.base_cidr_block)[1] >= 16 && split("/", var.base_cidr_block)[1] <= 28
+    error_message = "Your vpc cidr is not between 16 and 28"
+  }
+}
+
+variable "ami" {
+  description = "The AMI to use for the instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+}
+
+variable "name_tag" {
+  description = "The name tag for the resource"
+  type        = string
 }
