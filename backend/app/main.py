f0d0bebc0d54834f9d2bd4a38f1827aee5d96f58
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-17 15:02:16 -0400

    Add db table creation from models, root endpoint response

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+import models
+from .database import engine
+
 from .api.endpoints import health
 from .api.endpoints import leads
 from .api.endpoints import billing_reports
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 app.include_router(health.router, tags=["health"])
 app.include_router(leads.router, tags=["leads"])
 app.include_router(billing_reports.router, tags=["billing_reports"])
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
