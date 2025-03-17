9ec09e878b2219fc8123ac748c208fa7e134928e
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-17 14:04:53 -0400

    Add db_dependency for exporting to endpoint files

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,6 @@
-from sqalchemy import create_engine
+from typing import Annotated
+from fastapi import Depends
+from sqalchemy import create_engine, Session
 from sqalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,3 +9,12 @@ DATABASE_URL = "postgresql+asyncpg://postgres:password123@localhost:5432/Billing
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()  # This is the base class for declarative models
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]  # This is the dependency for FastAPI routes
