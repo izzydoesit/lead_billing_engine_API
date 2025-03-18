bad2a328fadb238f3f61e48e0d6ea2631e955992
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-18 17:40:21 -0400

    Update foreign keys and relationships in Models

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,40 +9,9 @@ class Base(DeclarativeBase):
     pass
 
 
-class Lead(Base):
-    __tablename__: str = "leads"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    customer_id: Mapped[int] = mapped_column(
-        ForeignKey("customers.id"), nullable=False, index=True
-    )
-    product_id: Mapped[int] = mapped_column(
-        ForeignKey("products.id"), nullable=False, index=True
-    )
-    lead_type: Mapped[str] = mapped_column(String(30), nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
-
-
-class Action(Base):
-    __tablename__: str = "actions"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    lead_id: Mapped[int] = mapped_column(ForeignKey("leads.id"), index=True)
-    customer_id: Mapped[int] = mapped_column(ForeignKey("customers.id"), index=True)
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), index=True)
-    lead_type: Mapped[str] = mapped_column(String(30), nullable=False)
-    action_type: Mapped[str] = mapped_column(String(30), nullable=False)
-    engagement_level: Mapped[str] = mapped_column(String(10), nullable=False)
-    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
-    billing_report_id: Mapped[int] = mapped_column(
-        ForeignKey("billing_reports.id"), index=True
-    )
-    cost_amount: Mapped[Optional[float]] = mapped_column(Float, default=None)
-    is_duplicate: Mapped[Optional[bool]] = mapped_column(Boolean, default=None)
-    status: Mapped[Optional[str]] = mapped_column(String(20), default=None)
-
-
 class Customer(Base):
     __tablename__: str = "customers"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
@@ -50,10 +19,45 @@ class Customer(Base):
 
 class Product(Base):
     __tablename__: str = "products"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False, index=True)
     description: Mapped[Optional[str]] = mapped_column(String(50), nullable=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+
+
+class Lead(Base):
+    __tablename__: str = "leads"
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+    customer_id: Mapped[str] = mapped_column(ForeignKey("customers.id"), index=True)
+    product_id: Mapped[str] = mapped_column(ForeignKey("products.id"), index=True)
+    lead_type: Mapped[str] = mapped_column(String(30), nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+
+    customer: "Customer" = relationship("Customer", back_populates="leads")
+    product: "Product" = relationship("Product", back_populates="leads")
+    actions: List["Action"] = relationship("Action", back_populates="leads")
+
+
+class Action(Base):
+    __tablename__: str = "actions"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    lead_id: Mapped[int] = mapped_column(ForeignKey("leads.id"), index=True)
+    customer_id: Mapped[str] = mapped_column(ForeignKey("customers.id"), index=True)
+    product_id: Mapped[str] = mapped_column(ForeignKey("products.id"), index=True)
+    lead_type: Mapped[str] = mapped_column(String(30), nullable=False)
+    action_type: Mapped[str] = mapped_column(String(30), nullable=False)
+    engagement_level: Mapped[str] = mapped_column(String(10), nullable=False)
+    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
+    cost_amount: Mapped[Optional[float]] = mapped_column(Float, default=None)
+    is_duplicate: Mapped[Optional[bool]] = mapped_column(Boolean, default=None)
+    status: Mapped[Optional[str]] = mapped_column(String(20), default=None)
+
+    lead: "Lead" = relationship("Lead", back_populates="actions")
+    customer: "Customer" = relationship("Customer", back_populates="actions")
+    product: "Product" = relationship("Product", back_populates="actions")
+    billing_report: "BillingReport" = relationship(
+        "BillingReport", back_populates="actions"
+    )
 
 
 class BillingReport(Base):
@@ -62,13 +66,13 @@ class BillingReport(Base):
     billing_date: Mapped[DateTime] = mapped_column(
         DateTime, default=func.now(), index=True
     )
-    customer_id: Mapped[int] = mapped_column(
-        ForeignKey("customers.id"), nullable=False, index=True
+    customer_id: Mapped[str] = mapped_column(ForeignKey("customer.id"), index=True)
+    customer_name: Mapped[str] = mapped_column(ForeignKey("customer.name"), index=True)
+    customer_email: Mapped[str] = mapped_column(ForeignKey("customer.email"))
+    reported_actions: Mapped[List[Action]] = relationship(
+        "Action", back_populates="billingreport"
     )
-    customer_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    customer_email: Mapped[str] = mapped_column(String(50), nullable=False)
-    actions: Mapped[List["Action"]] = relationship()
-    # list of dicts with product_id, product_name and total_amount
-    totals_by_product: Mapped[Optional[List[Dict[int, any]]]] = mapped_column(JSON)
-    total_amount: Mapped[Optional[float]] = mapped_column(Float, nullable=False)
+    # FIXME: list of dicts with product_id, product_name and total_amount
+    totals_by_product: Mapped[Optional[List[Dict[str, float]]]] = mapped_column(JSON)
+    total_amount: Mapped[float] = mapped_column(Float, nullable=False)
     savings_amount: Mapped[Optional[float]] = mapped_column(Float, default=0.0)
