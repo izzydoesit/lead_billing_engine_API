b9d8a243cc0f1f4d58ba194b6983e37ab38b3b20
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-20 16:37:39 -0400

    Add an all method to /models/init file for wildcard imports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,3 +4,12 @@ from .product import Product
 from .lead import Lead
 from .action import Action
 from .billing_report import BillingReport
+
+__all__ = [
+    "ModelBase",
+    "Customer",
+    "Product",
+    "Lead",
+    "Action",
+    "BillingReport",
+]
