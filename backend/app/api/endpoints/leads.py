53bbf91c0e9e61139a71e8ff502bfa5560a3a575
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-22 20:56:18 -0400

    Fix endpoint errors for leads

--- backend/app/api/endpoints/leads.py
+++ backend/app/api/endpoints/leads.py
@@ -2,22 +2,26 @@ from http.client import HTTPResponse
 from typing import List, Optional, Union, Annotated
 from fastapi import APIRouter, HTTPException, Depends, Query
 from pydantic import BaseModel, Field
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
+from sqlalchemy.exc import SQLAlchemyError, NoResultFound
+from sqlalchemy.ext.asyncio import AsyncSession
 import logging
 from app.core.database import get_async_session
-import app.models as models
+from app.models import (
+    Lead as LeadModel,
+    Customer as CustomerModel,
+    Product as ProductModel,
+)
 from app.schemas import (
     CustomerCreate,
-    Customer,
-    Lead,
+    Customer as CustomerSchema,
     LeadCreate,
+    Lead as LeadSchema,
     ProductCreate,
-    Product,
-    LeadCreate,
+    Product as ProductSchema,
     ActionCreate,
-    Action,
+    Action as ActionSchema,
 )
 from app.crud import calculate_action_value
 from app.crud.leads_service import (
@@ -25,64 +29,84 @@ from app.crud.leads_service import (
     save_action_in_database,
 )
 from app.shared import LeadTypes, LEAD_ACTION_COSTS
+from app.shared.utils import to_dict
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 
-@router.get("/leads", response_model=Lead)
+@router.get("/leads", response_model=List[LeadSchema])
 async def get_leads(
     db: Annotated[AsyncSession, Depends(get_async_session)],
     customer_id: Optional[str] = Query(None, description="Filter by customer ID"),
     product_id: Optional[str] = Query(None, description="Filter by product ID"),
     lead_type: Optional[LeadTypes] = Query(None, description="Filter by lead type"),
-    session: AsyncSession = Depends(get_async_session),
 ):
     try:
-        query = select(Lead)
+        query = select(LeadModel).options(
+            selectinload(LeadModel.customer),
+            selectinload(LeadModel.product),
+            selectinload(LeadModel.actions),
+        )
 
         if customer_id:
-            query = query.where(Lead.customer_id == customer_id)
+            query = query.where(LeadModel.customer_id == customer_id)
         if product_id:
-            query = query.where(Lead.product_id == product_id)
+            query = query.where(LeadModel.product_id == product_id)
         if lead_type:
-            query = query.where(Lead.lead_type == lead_type)
+            query = query.where(LeadModel.lead_type == lead_type)
 
-        result = await session.execute(query)
+        logger.info(f"~~**~~ GET /leads querying database... QUERY: {query}")
+        result = await db.execute(query)
         leads = result.scalars().all()
+        logger.info(f"~~**~~ GET /leads finished querying db. RESULT: {leads}")
 
         return leads
 
     except SQLAlchemyError as e:
-        logging.error(f"Database query error: {str(e)}")
+        logger.error(f"Database query error: {str(e)}")
         raise HTTPException(status_code=500, detail="Database query error")
 
     except Exception as e:
-        logging.error(f"Unexpected error: {str(e)}")
+        logger.error(f"Unexpected error: {str(e)}")
         raise HTTPException(status_code=500, detail="Unexpected error")
 
 
-@router.get("/leads/{lead_id}", response_model=Lead)
+@router.get("/leads/{lead_id}", response_model=LeadSchema)
 async def get_one_lead(
-    lead_id: int, db: Annotated[AsyncSession, Depends(get_async_session)]
+    lead_id: str, db: Annotated[AsyncSession, Depends(get_async_session)]
 ):
-    # async with AsyncSessionLocal() as db:
-    #     result = await db.execute(select(Leads)).where(Leads.lead_id == lead_id).first()
-    #     if result is None:
-    #         raise HTTPException(status_code=404, detail="Lead not found")
-    #     return result
+    try:
+        logger.info(f"~~**~~ GET /leads/{lead_id} querying database...")
+        query = (
+            select(LeadModel)
+            .options(
+                selectinload(LeadModel.customer),
+                selectinload(LeadModel.product),
+                selectinload(LeadModel.actions),
+            )
+            .where(LeadModel.id == lead_id)
+        )
+        result = await db.execute(query)
+        lead_db = result.scalar_one()
+        logger.info(
+            f"~~**~~ GET /leads/{lead_id} finished querying db. RESULT: {lead_db}"
+        )
 
-    # try:
-    #     result = await session.get(Leads, lead_id)
-    #     if result is None:
-    #         raise NoResultFound
-    #     return result
-    # except NoResultFound:
-    #     raise HTTPException(status_code=404, detail="Item not found", headers={"X-Error": "Not-Found"})
+        if lead_db is None:
+            raise HTTPException(
+                status_code=404,
+                detail="Lead not found",
+                headers={"X-Error": "Not-Found"},
+            )
 
-    result = await db.get(models.Lead, lead_id)
-    if result is None:
-        raise HTTPException(status_code=404, detail="Lead not found")
-    return result
+        lead_dict = to_dict(lead_db)
+        logger.info(f"~~**~~ Lead object: {lead_dict}")
+
+        return LeadSchema.from_orm(lead_db)
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        raise HTTPException(status_code=500, detail="Unexpected error")
 
 
 @router.post("/leads/", status_code=201)
@@ -98,9 +122,9 @@ async def create_lead(
         for action_data in lead_actions:
             # NOTE: db_action = Action(**action.dict())
             calculated_lead_action_value = calculate_action_value(
-                source=lead_data.lead_type,
-                result=action_data.action_type,
-                quality=action_data.engagement_type,
+                lead_type=lead_data.lead_type,
+                action_type=action_data.action_type,
+                engagement_level=action_data.engagement_level,
             )
             action_data.cost_amount = calculated_lead_action_value
 
@@ -109,23 +133,23 @@ async def create_lead(
     return HTTPResponse(201)
 
 
-@router.post("/customers/", response_model=Customer, status_code=201)
+@router.post("/customers/", response_model=CustomerSchema, status_code=201)
 async def create_customer(
     customer_data: CustomerCreate,
     db: Annotated[AsyncSession, Depends(get_async_session)],
-) -> models.Customer:
-    db_customer = models.Customer(name=customer_data.name, email=customer_data.email)
+) -> CustomerModel:
+    db_customer = CustomerModel(name=customer_data.name, email=customer_data.email)
     db.add(db_customer)
     await db.commit()
     await db.refresh(db_customer)
     return db_customer
 
 
-@router.post("/products/", response_model=Product, status_code=201)
+@router.post("/products/", response_model=ProductSchema, status_code=201)
 async def create_product(
     product_data: ProductCreate, db: Annotated[AsyncSession, Depends(get_async_session)]
 ):
-    db_product = models.Product(**product_data.model_dump())
+    db_product = ProductModel(**product_data.model_dump())
     db.add(db_product)
     await db.commit()
     await db.refresh(db_product)
