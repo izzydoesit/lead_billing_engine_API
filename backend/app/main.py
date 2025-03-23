164d0bf639eccb536acff436f6e645caa24e0bcf
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-03-23 14:13:42 -0400

    Remove lifespan event from main

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import logging
 from fastapi_utils.tasks import repeat_every
 from alembic import context, config
+import asyncio
 from .config import settings
 from .models import ModelBase
 from .core.database import (
@@ -10,29 +11,17 @@ from .core.database import (
     check_database_status,
     drop_and_create_tables,
     seed_database,
+    apply_migrations,
 )
 from .api.endpoints import health
 from .api.endpoints import leads
+from .api.endpoints import billing_reports
 
-# FIXME: add from .api.endpoints import billing_reports
-from contextlib import asynccontextmanager
-import asyncio
 
 logging.basicConfig(level=logging.INFO)
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup logic
-    await check_database_status()
-    await drop_and_create_tables()
-    yield
-    # Shutdown logic
-    logging.info("Application shutdown.")
-
-
 app = FastAPI(
-    lifespan=lifespan,
     title="Lead Billing API",
     description="API for processing billing of customer leads",
     version="0.1.0",
@@ -55,30 +44,23 @@ else:
     )
 app.include_router(health.router, tags=["health"])
 app.include_router(leads.router, tags=["leads"])
-# app.include_router(billing_reports.router, tags=["billing_reports"])
-
-
-@app.on_event("startup")
-@repeat_every(seconds=600)  # Run every 10 minutes
-async def startup_event():
-    check_database_status()
+app.include_router(billing_reports.router, tags=["billing_reports"])
 
 
 @app.on_event("startup")
 async def startup_event():
-    logging.info("Running startup event...")
+    logging.info("~~**~~ Running startup event...")
     await check_database_status()
-    # await run_migrations()
+    await drop_and_create_tables()
+    # await apply_migrations()
     await seed_database()
-
-    app.add_event_handler("startup", check_database_status)
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    logging.info("Running shutdown event...")
+    logging.info("~~**~~ Running shutdown event...")
     await async_engine.dispose()
-    logging.info("Engine disposed.")
+    logging.info("~~**~~ Engine disposed.")
 
 
 @app.get("/")
